7cadf7a3a4e7f9e458a2711ffb465f2fd83efd29
Aj Fodera <hlafsyp@gmail.com>
2022-06-05 00:19:14 -0700

    Added Skinning Module

---
+++ Modules/Skin/Skinner.lua
@@ -0,0 +1,7 @@
+local Cinnabar, Util, Cfg, Module = unpack(select(2,...))
+
+-- Method of skinning is taken from ElvUI
+-- Their way of skinning everything is way more time efficient
+-- than writing a custom skin for every blizzard element and all newly created elements
+
+
