74642d56c72bb736d5dd821311d0e6140e54886c
Aj Fodera <hlafsyp@gmail.com>
2022-06-05 00:18:51 -0700

    Added Skinning Module Changed MAX_LEVEL constant to use GetMaxPlayerLevel instead of hard coded integer

--- Initialize.lua
+++ Initialize.lua
@@ -13,26 +13,28 @@ local m = {}
 m["UnitFrames"] = Cinnabar:NewModule("UnitFrames")
 m["Tooltip"]    = Cinnabar:NewModule("Tooltip")
 m["Databar"]    = Cinnabar:NewModule("Databar")
+m['Skin']       = Cinnabar:NewModule("Skin")
+
 -------------
 --  Extra  --
 -------------
 Cinnabar.data = {
-    NAME = addon_name,
-    MAX_LEVEL = 60,
-    Mount = {
-        IDs = {},
-        SpellIDs = {},
-    },
-    COLORS = {
-        UI_FG       = {r = 1, g = 0.88, b = 0.85,hex = "ffffe1da"},
-        UI_PRIMARY  = {r = 0.89, g = 0.3, b = 0.18,hex = "ffE44D2E"},
-    },
+  NAME = addon_name,
+  MAX_LEVEL = GetMaxPlayerLevel(),
+  Mount = {
+    IDs = {},
+    SpellIDs = {},
+  },
+  COLORS = {
+    UI_FG       = {r = 1, g = 0.88, b = 0.85,hex = "ffffe1da"},
+    UI_PRIMARY  = {r = 0.89, g = 0.3, b = 0.18,hex = "ffE44D2E"},
+  },
 }
 
 ------------
 -- Assign --
 ------------
-core[1] = Cinnabar   -- Core Table, holds central functions to the addon used in the majority of modules
+core[1] = Cinnabar  -- Core Table, holds central functions to the addon used in the majority of modules
 core[2] = {}        -- Utility Table, any debug stuff or other utility functions will be placed in here
 core[3] = config    -- Configuration Table, holds all config data
 core[4] = m         -- Modules Table, Holds all Module tables
